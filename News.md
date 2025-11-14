78356a1cccd41b60fe4bec8ca27962ce5140936d
Gleb Iagelskii <gleb.iagelskii@icloud.com>
2025-11-14 11:08:03 +0000

    Differences functon in temp_conversion.py and new 'Wodoo Board / Comments stuff' added

---
+++ News.md
@@ -0,0 +1,1 @@
+This is a space to add news on repository that are not quite as important as those in the general systems message file. Do please leave your comments, plan and updates as well as ideas!
--- temp_conversion.py
+++ temp_conversion.py
@@ -8,3 +8,14 @@ def FK(x):
     y = Fc(x)
     z = y + 273.15
     return z
+
+def differencest(temp_list):
+	differences = []
+	for i in range(len(temp_list)):
+		if i>0 and i<len(temp_list):
+			differences.append(temp_list[i]-temp_list[i-1])
+			print(f'Difference is {temp_list[i]-temp_list[i-1]}')
+		else:
+			pass
+	return differences
+	
